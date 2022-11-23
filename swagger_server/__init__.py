33b177ced774ebaf450b72e61c04b293ad0a97e8
Michael Tang <michaeltang6649@gmail.com>
2022-11-23 12:18:14 -0500

    cors

--- swagger_server/__init__.py
+++ swagger_server/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 import connexion
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 from swagger_server import encoder
 
 db = SQLAlchemy()
@@ -13,6 +13,7 @@ def create_app():
     logging.basicConfig(level=logging.DEBUG)
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.config.from_envvar("CONFIG")
+    CORS(app.app, resources={"*": {"origins": "*"}})
     print(app.app.config)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Bespoked Bikes - API'}, pythonic_params=True)
