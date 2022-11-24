2b56adb3090c5815fb51d209a32836ceb578629c
Michael Tang <michaeltang6649@gmail.com>
2022-11-23 23:39:11 -0500

    update

---
+++ swagger_server/controllers/default_controller.py
@@ -0,0 +1,15 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def health_check():  # noqa: E501
+    """health check
+
+    health check # noqa: E501
+
+
+    :rtype: str
+    """
+    return 'Hello'
